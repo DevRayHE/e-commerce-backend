679907b725ea6b4289091fab33be073f08a6f7f3
Ray He <devrayhe@gmail.com>
2021-11-20 22:02:06 +1100

    Product, Tag and ProductTag Database Model
    Basic structure completed

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    prodct_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+        unique: fase
+      }
+    },
+    location_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'location',
+        key: 'id',
+        unique: fase
+      }
+    },
   },
   {
     sequelize,
